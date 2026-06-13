278c7e1fa26875366be95565b1bf7eee7ba5be17
Rakesh_790 <brakeshroshan098@gmail.com>
2026-06-13 12:09:25 +0530

    learn about searching and filtering

--- spotify project/src/utils/paginate.js
+++ spotify project/src/utils/paginate.js
@@ -1,12 +1,22 @@
-const paginate = async (model, query = {}, reqQuery = {}) => {
+const paginate = async (model, query = {}, reqQuery = {}) => {  // model is db name, query is clause like find email basis, reqQuery is page and limit number.
+
     const page = Math.max(Number(reqQuery.page) || 1, 1);
     const limit = Math.max(Number(reqQuery.limit) || 5, 1);
 
     const skip = (page - 1) * limit;
 
-    const totalRecords = await model.countDocuments(query);
+    let filter = { ...query };
 
-    const data = await model.find(query)
+    if(reqQuery.search){
+        filter.title = {
+            $regex: reqQuery.search,
+            $options: "i"
+        };
+    };
+
+    const totalRecords = await model.countDocuments(filter);
+
+    const data = await model.find(filter)
         .skip(skip)
         .limit(limit);
 
